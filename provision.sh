cf02295aa6bbdc5e73b0c2f46317a139408a7557
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2020-04-22 11:22:21 +0200

    Suppression du dossier utilisateur "ninjam", qui ne sert à rien

--- provision.sh
+++ provision.sh
@@ -2,14 +2,15 @@
 
 # Générer le mot de passe crypté à l'aide de cette commande :
 # mkpasswd -sha256
-useradd -m -p "$6$yYuKAckgiH1DSaT1$pynCbNOuFlt.cyrrvrqLPMQZlI5B/AjdznpZ2PfvAXwYwDpFFjveDgVLE6YLnxz4LQPPgjkcYHdX/cNXI61Li0" -s /bin/bash ninjam
+useradd ninjam
 apt update
 apt install git -y
-git clone https://www-dev.cockos.com/ninjam/ninjam.git /home/ninjam/ninjam
+git clone https://www-dev.cockos.com/ninjam/ninjam.git
 apt install build-essential -y
-make -C /home/ninjam/ninjam/ninjam/server
-sudo cp /home/ninjam/ninjam/ninjam/server/ninjamsrv /usr/local/bin
-cp /vagrant/ninjam.cfg /home/ninjam/
+make -C ninjam/ninjam/server
+sudo cp ninjam/ninjam/server/ninjamsrv /usr/local/bin
+sudo mkdir /etc/ninjam
+cp /vagrant/ninjam.cfg /etc/ninjam
 cp /vagrant/ninjam.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable ninjam
