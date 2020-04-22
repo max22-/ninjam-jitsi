8a523b4da99b8655673d8c5c8e5df07670eb4f74
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2020-04-22 21:56:37 +0200

    Suppression des "sudo" qui sont inutiles car le provisioning est fait en tant que root

--- provision.sh
+++ provision.sh
@@ -6,23 +6,23 @@ apt install git -y
 git clone https://www-dev.cockos.com/ninjam/ninjam.git
 apt install build-essential -y
 make -C ninjam/ninjam/server
-sudo cp ninjam/ninjam/server/ninjamsrv /usr/local/bin
-sudo mkdir /etc/ninjam
+cp ninjam/ninjam/server/ninjamsrv /usr/local/bin
+mkdir /etc/ninjam
 cp /vagrant/ninjam.cfg /etc/ninjam
 cp /vagrant/ninjam.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable ninjam
-sudo systemctl start ninjam
+systemctl daemon-reload
+systemctl enable ninjam
+systemctl start ninjam
 
 # PulseAudio
 echo "Installation de PulseAudio"
-sudo apt install pulseaudio -y
+apt install pulseaudio -y
 
 # Jackd
 echo "Installation de Jackd"
 DEBIAN_FRONTEND=noninteractive apt install jackd pulseaudio-module-jack -yq
 
 # Client faisant le lien entre ninjam et jack
-#sudo apt install libncurses-dev libasound2-dev libjack-dev libvorbis-dev -y
-#make -C ninjam/ninjam/cursesclient
-#cp ninjam/ninjam/cursesclient/cninjam /usr/local/bin
+apt install libncurses-dev libasound2-dev libjack-dev libvorbis-dev -y
+make -C ninjam/ninjam/cursesclient
+cp ninjam/ninjam/cursesclient/cninjam /usr/local/bin
